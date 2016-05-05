2a16c04510eb37124738adc7b86d6fda540098a1
Nuh <hex-tr@hotmail.com>
2016-05-05 13:15:23 +0300

    Add project files.

---
+++ AspNetMvcCRUD/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(AspNetMvcCRUD.Startup))]
+namespace AspNetMvcCRUD
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
